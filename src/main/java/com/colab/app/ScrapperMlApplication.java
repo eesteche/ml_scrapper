3e295f1684c04a3543b0d7211e989f9856ec0462
Edgar Emanuel Esteche Arzamendia <esteche718@gmail.com>
2023-04-22 12:14:13 -0300

    first commit

---
+++ src/main/java/com/colab/app/ScrapperMlApplication.java
@@ -0,0 +1,13 @@
+package com.colab.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapperMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapperMlApplication.class, args);
+	}
+
+}
